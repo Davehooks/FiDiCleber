43ab83a90d10e2684c2d99a7fe9028bfb4c949ce
Davehooks <150075150+Davehooks@users.noreply.github.com>
2025-11-05 12:27:08 -0300

    CineMachineStarted

---
+++ TCQ-25/Assets/Core/Scripts/Camera/CameraFollowObject.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CameraFollowObject : MonoBehaviour
+{
+    [SerializeField] private PlayerController Player;
+    [SerializeField] private Transform PlayerTransform;
+
+    [SerializeField] private float _flipYRotationTime = 0.5f;
+
+    private Coroutine _turnCoroutine;
+
+    private bool _IsFacingRight;
+
+
+    private void Start()
+    {
+        PlayerTransform = Player.transform;
+        _IsFacingRight = Player._isFacingRight;
+
+    }
+
+    public void CallTurn()
+    {
+        LeanTween.rotateY(gameObject, DetermineEndRotation(), _flipYRotationTime).setEaseInOutSine();
+    }
+
+    private void Update()
+    {
+        transform.position = PlayerTransform.position;
+    }
+
+
+    private float DetermineEndRotation()
+    {
+        _IsFacingRight = !_IsFacingRight;
+
+        if (!_IsFacingRight)
+        {
+            return 180f;
+        }
+
+        else
+        {
+            return 0f;
+        }
+    }
+}
